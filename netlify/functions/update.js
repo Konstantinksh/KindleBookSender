c2dc9efc2a51a029d1260d9f7c52639c83adff14
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-06-15 18:47:53 +0300

    experiment

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -6,9 +6,13 @@ exports.handler = async (event) => {
 
   await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
     chat_id: JSON.parse(event.body).message.chat.id,
-    text: "I got your message!",
+    text: "I got your message!" + `${JSON.parse(event.body).message.chat.id}`,
   });
-  
+
+  await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/getFile`, {
+    file_id: AgACAgIAAxkBAAMVYqn86eifM0LAmiW4jfwFzIo-R1wAAqC9MRtZylBJmnucL_3_8gcBAAMCAANtAAMkBA
+  });
+
   return { statusCode: 200 };
 }
 
