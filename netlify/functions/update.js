96e0a144434e250a6903ddadd1e3e6aa62c19b63
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-06-15 17:37:27 +0300

    Respond to messages

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,4 +1,15 @@
+const axios = require("axios").default;
+
 exports.handler = async (event) => {
+  
   console.log("Received an update from Telegram!", event.body);
+
+  await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+    chat_id: JSON.parse(event.body).message.chat.id,
+    text: "I got your message!",
+  });
+  
   return { statusCode: 200 };
 }
+
+
