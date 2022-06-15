f6c3c71e1b10f501d8143514bef044b21dea306c
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-06-15 20:36:03 +0300

    delete wrong function

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
 
   await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
     chat_id: JSON.parse(event.body).message.chat.id,
-    text: "I got your message!" + `${JSON.parse(event.body)}`,
+    text: "I got your message!" + `${JSON.parse(event.body).message}`,
   });
 
 
