17ad030276ad41e3192659145106db2dd24cc9bb
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-06-15 17:29:56 +0300

    feat: impement sendmessage function

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,4 +1,16 @@
-exports.handler = async (event) => {
-  console.log("Received an update from Telegram!", event.body);
-  return { statusCode: 200 };
-};
+// exports.handler = async (event) => {
+//   console.log("Received an update from Telegram!", event.body);
+//   return { statusCode: 200 };
+// };
+
+const TelegramApi = require('node-telegram-bot-api')
+
+const token = process.env.BOT_TOKEN;
+
+const bot  = new TelegramApi(token, {polling: true})
+
+bot.on('message', msg => {
+  const text = msg.text;
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, text)
+})
