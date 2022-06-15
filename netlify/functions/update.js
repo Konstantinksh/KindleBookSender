2e13c2e4c0c97503a8130f898c814dc459e32f5e
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-06-15 17:33:18 +0300

    feat: delete preveous function

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,16 +1,4 @@
-// exports.handler = async (event) => {
-//   console.log("Received an update from Telegram!", event.body);
-//   return { statusCode: 200 };
-// };
-
-const TelegramApi = require('node-telegram-bot-api')
-
-const token = process.env.BOT_TOKEN;
-
-const bot  = new TelegramApi(token, {polling: true})
-
-bot.on('message', msg => {
-  const text = msg.text;
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, text)
-})
+exports.handler = async (event) => {
+  console.log("Received an update from Telegram!", event.body);
+  return { statusCode: 200 };
+}
