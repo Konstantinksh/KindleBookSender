1cae6c03b09039c8772b07bfff27c1f2a9bf8456
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-06-16 08:30:07 +0300

    test and study

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,7 +1,7 @@
 const axios = require("axios").default;
 
 exports.handler = async (event) => {
-  console.log("Received an update from Telegram!", JSON.parse(event.body));  
+  console.log("Received an update from Telegram!", JSON.parse(event));  
 
   return { statusCode: 200 };
 }
