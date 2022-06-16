951230a380260cf21f61a1a814f8d96cbe79fad9
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-06-16 22:54:52 +0300

    test

--- sendBook.js
+++ sendBook.js
@@ -11,33 +11,32 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-module.exports = (fileLink, fileNameRu) => {
-
-  const mailConfigurations = {
+const mailConfigurations = {
     
-    // It should be a string of sender email
-    from: 'alfredthehelperbot@gmail.com',
-      
-    // Comma Separated list of mails
-    to: 'konstantinksh@gmail.com',
+  // It should be a string of sender email
+  from: 'alfredthehelperbot@gmail.com',
+    
+  // Comma Separated list of mails
+  to: 'konstantinksh@gmail.com',
 
-    // Subject of Email
-    subject: 'Sending Email using Node.js',
-      
-    // This would be the text of email body
-    text: 'Hi! There, You know I am using the'
-      + ' NodeJS Code along with NodeMailer '
-      + 'to send this email.',
+  // Subject of Email
+  subject: 'Sending Email using Node.js',
+    
+  // This would be the text of email body
+  text: 'Hi! There, You know I am using the'
+    + ' NodeJS Code along with NodeMailer '
+    + 'to send this email.',
 
-    attachments: [
-      {
-        // use URL as an attachment
-        filename: `fileNameRu`,
-        path: `https://api.telegram.org/file/bot5496988316:AAFNenPKWXTDX9NdAlCYj3Z8WCmDY285ky4/documents/file_3.epub`
-      }
-    ]
-  };
+  attachments: [
+    {
+      // use URL as an attachment
+      filename: `fileNameRu.txt`,
+      path: `https://api.telegram.org/file/bot5496988316:AAFNenPKWXTDX9NdAlCYj3Z8WCmDY285ky4/documents/file_3.epub`
+    }
+  ]
+};
 
+module.exports = (fileLink, fileNameRu) => {
   transporter.sendMail(mailConfigurations, function(error, info){
     if (error) throw Error(error);
       console.log('Email Sent Successfully');
