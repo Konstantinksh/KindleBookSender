8144500b86ae051dc148a660bc04e31a1ea3b9e8
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-06-16 08:05:09 +0300

    test and study

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -3,31 +3,31 @@ const axios = require("axios").default;
 exports.handler = async (event) => {
   console.log("Received an update from Telegram!", JSON.parse(event.body));  
 
-  if (JSON.parse(event.body).message) {
-    await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-      chat_id: JSON.parse(event.body).message.chat.id,
-      text: "I got your message!",
-    });
-  
-
-      if (JSON.parse(event.body).message.document) {
-
-        const fileID = JSON.parse(event.body).message.document.file_id
-
-        if (fileID) {
-          const fileToDownload = await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/getFile`, {
-            file_id: fileID
-          });
-
-          console.log(fileToDownload.data.result)
-
-          await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-            chat_id: JSON.parse(event.body).message.chat.id,
-            text: `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${fileToDownload.data.result.file_path}`,
-          });
-        }
-      }
-  }
-
   return { statusCode: 200 };
 }
+
+// if (JSON.parse(event.body).message) {
+//   await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+//     chat_id: JSON.parse(event.body).message.chat.id,
+//     text: "I got your message!",
+//   });
+
+
+//     if (JSON.parse(event.body).message.document) {
+
+//       const fileID = JSON.parse(event.body).message.document.file_id
+
+//       if (fileID) {
+//         const fileToDownload = await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/getFile`, {
+//           file_id: fileID
+//         });
+
+//         console.log(fileToDownload.data.result)
+
+//         await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+//           chat_id: JSON.parse(event.body).message.chat.id,
+//           text: `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${fileToDownload.data.result.file_path}`,
+//         });
+//       }
+//     }
+// }
